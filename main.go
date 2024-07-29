03f8759a2c29f40d63ee84de46842bd61479d58b
brenoacf <brenoacf@gmail.com>
2024-07-29 14:17:43 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import "github.com/brenoacf/gotags-api-test/router"
+import (
+	"github.com/brenoacf/gotags-api-test/config"
+	"github.com/brenoacf/gotags-api-test/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
 	// Initialize Router
 	router.Initialize()
 }
