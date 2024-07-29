5430c3e4e3cd87e84a100a8b268bdb4acc69a4b1
brenoacf <brenoacf@gmail.com>
2024-07-29 18:01:25 -0300

    feat: create a json response type for tags

--- schemas/tags.go
+++ schemas/tags.go
@@ -1,6 +1,8 @@
 package schemas
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
@@ -11,4 +13,16 @@ type Tag struct {
 	Date  string
 	Hour  string
 	Value float64
+}
+
+type TagResponse struct {
+	ID        uint      `json:"id"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	DeletedAt time.Time `json:"deletedAt,omitempty"`
+	Code      uint64    `json:"code"`
+	Tag       string    `json:"tag"`
+	Date      string    `json:"date"`
+	Hour      string    `json:"hour"`
+	Value     float64   `json:"value"`
 }
