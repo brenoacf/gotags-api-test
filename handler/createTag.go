e24ae41260739d989017a8f74cd056e745aaecc3
brenoacf <brenoacf@gmail.com>
2024-07-31 11:19:43 -0300

    docs: add documentation for create tags

--- handler/createTag.go
+++ handler/createTag.go
@@ -7,6 +7,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// @BasePath /api/v1
+
+// @Summary Create Tag
+// @Description Create a new tag
+// @Tags Tags
+// @Accept json
+// @Produce json
+// @Param request body CreateTagRequest true "Request body"
+// @Success 200 {object} CreateTagResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /tag [post]
 func CreateTagHandler(ctx *gin.Context) {
 	request := CreateTagRequest{}
 
