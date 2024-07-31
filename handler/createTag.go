fd939c1711e9d3325bba088e67ba529ca511b3ab
brenoacf <brenoacf@gmail.com>
2024-07-30 23:26:30 -0300

    feat: add createTag handler logic

--- handler/createTag.go
+++ handler/createTag.go
@@ -3,11 +3,34 @@ package handler
 import (
 	"net/http"
 
+	"github.com/brenoacf/gotags-api-test/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateTagHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "POST Tag",
-	})
+	request := CreateTagRequest{}
+
+	ctx.BindJSON(&request)
+
+	if err := request.validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	tag := schemas.Tag{
+		Code:  request.Code,
+		Tag:   request.Tag,
+		Date:  request.Date,
+		Hour:  request.Hour,
+		Value: request.Value,
+	}
+
+	if err := db.Create(&tag).Error; err != nil {
+		logger.Errorf("error creating tag: %+v", err)
+		sendError(ctx, http.StatusInternalServerError, "error creating tag on database")
+		return
+	}
+
+	sendSuccess(ctx, "create-tags", tag)
 }
